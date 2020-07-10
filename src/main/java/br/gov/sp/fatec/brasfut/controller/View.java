af353dc47e4d388001cb20c605ac092eaac762c3
juliosrangel <julio.cesar.s.rangel@gmail.com>
2020-07-10 19:29:09 -0300

    Update View.java

--- src/main/java/br/gov/sp/fatec/brasfut/controller/View.java
+++ src/main/java/br/gov/sp/fatec/brasfut/controller/View.java
@@ -2,9 +2,9 @@ package br.gov.sp.fatec.brasfut.controller;
 
 public class View {
 
-	public static class FraseResumo{}
+	public static class JogadorResumo{}
 	
-	public static class FraseCompleta extends FraseResumo{
+	public static class JogadorCompleta extends JogadorResumo{
 		
 	}
 }
